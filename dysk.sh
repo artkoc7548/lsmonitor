5f54db868295bb0ea3eb89be02994fab37365b97
artkoc7548 <artur@koczwara.net.pl>
2018-05-22 08:50:49 +0200

    Add files via upload

---
+++ dysk.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#Filename: disklog.sh
+#Description: Monitor disk usage health for remote systems
+
+
+logfile="diskusage.log"
+
+if [ -n $1 ]
+then
+  logfile=$1
+fi
+
+if [ ! -e $logfile ]
+then
+  printf "%-8s %-14s %-9s %-8s %-6s %-6s %-6s %s\n" "Date" "IP address" "Device" "Capacity" "Used" "Free" "Percent" "Status" > $logfile
+fi
+
+df -H | grep ^/dev/ >> $logfile
+
+cur_date=$(date +%D)
+#printf "%-8s %-14s " $cur_date $ip
+
+# echo $line | awk '{ printf("%-9s %-8s %-6s %-6s %-8s",$1,$2,$3,$4,$5); }'
+# pusg=$(echo $line | egrep -o "[0-9]+%")
+# pusg=${pusg/\%/};
+
+#  if [ $pusg -lt 80 ];
+#  then
+#    echo SAFE
+#  else
+#    echo ALERT
+#  fi
+
